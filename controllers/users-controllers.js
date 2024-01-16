34e244b76f408b3b01f6e9d6f082c09daba642bb
echofreedom007 <b.zhang41@outlook.com>
2024-01-16 15:58:33 -0500

    feat: login with hashed password

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -97,12 +97,31 @@ const login = async (req, res, next) => {
     return next(error);
   }
 
-  if (!identifiedUser || identifiedUser.password !== password) {
+  if (!identifiedUser) {
     const error = new HttpError(
       "Could not identify user, credentials seem to be wrong",
       401
     );
 
+    return next(error);
+  }
+
+  let isValidPassword = false;
+  try {
+    isValidPassword = await bcrypt.compare(password, identifiedUser.password);
+  } catch (err) {
+    const error = new HttpError(
+      "Could not log you in, please check your credentials and try again.",
+      500
+    );
+    return next(error);
+  }
+
+  if (!isValidPassword) {
+    const error = new HttpError(
+      "Invalid credentials, could not log you in.",
+      401
+    );
     return next(error);
   }
 
